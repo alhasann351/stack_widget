d05fcfaef8821a73ac302793b583e6a47d506dba
alhasann351 <alhasann352@gmail.com>
2024-02-23 21:42:15 +0600

    simple stack and position combination

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,49 +38,173 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Center(
-        child: Stack(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              color: Colors.green,
-              height: 300,
-              width: 300,
-              alignment: Alignment.bottomRight,
-              child: const Text(
-                'Container 1',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            Stack(
+              children: [
+                Container(
+                  color: Colors.green,
+                  height: 100,
+                  width: 100,
+                  alignment: Alignment.bottomRight,
+                  child: const Text(
+                    '1',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-              ),
+                Container(
+                  color: Colors.red,
+                  height: 80,
+                  width: 80,
+                  alignment: Alignment.bottomRight,
+                  child: const Text(
+                    '2',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 60,
+                  width: 60,
+                  alignment: Alignment.bottomRight,
+                  child: const Text(
+                    '3',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              color: Colors.red,
-              height: 250,
-              width: 250,
-              alignment: Alignment.bottomRight,
-              child: const Text(
-                'Container 2',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
+            const SizedBox(
+              height: 15,
             ),
-            Container(
-              color: Colors.blue,
-              height: 200,
-              width: 200,
-              alignment: Alignment.bottomRight,
-              child: const Text(
-                'Container 3',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            Stack(
+              children: [
+                Container(
+                  color: Colors.pink,
+                  height: 250,
+                  width: 250,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'Container',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-              ),
+                Positioned(
+                  left: 0,
+                  child: Container(
+                    color: Colors.purple,
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Container 1',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  bottom: 0,
+                  child: Container(
+                    color: Colors.blue,
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Container 2',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    color: Colors.green,
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Container 3',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                    color: Colors.orange,
+                    height: 100,
+                    width: 100,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Container 4',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Image.network(
+                  'https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2022/07/Superman-Animated.jpg',
+                  fit: BoxFit.fill,
+                  height: 180,
+                  width: 300,
+                ),
+                const Positioned(
+                  bottom: -28,
+                  right: 0,
+                  left: 0,
+                  child: CircleAvatar(
+                    backgroundColor: Color(0xFFF72798),
+                    radius: 30,
+                    child: Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
